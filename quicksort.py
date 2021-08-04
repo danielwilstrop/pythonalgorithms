43cbc76d7c7fce0ea0895dcbc92b9a3c8e0c5d81
Daniel Wilstrop <daniel.wilstrop@outlook.com>
2021-08-04 22:09:12 +0100

    Quicksort test with strings

--- quicksort.py
+++ quicksort.py
@@ -10,7 +10,6 @@ def quicksort(array):
 
         return quicksort(less) + [pivot] + quicksort(greater)
         
-
 #Tests
 print(quicksort([34,56,2,3,45])) #Prints [2, 3, 34, 45, 56]
-print(quicksort([10,9,8,7,6,5,4,3,2,1,0])) #Prints [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(quicksort(['jas', 'becka', 'dan', 'alice'])) #Prints ['alice', 'becka', 'dan', 'jas']
