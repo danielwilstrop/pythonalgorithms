f53b35413478b3f7afb2892a3942f31bdfb57b71
Daniel Wilstrop <daniel.wilstrop@outlook.com>
2021-08-04 22:00:46 +0100

    Enhanced quicksort with a random pivot

--- quicksort.py
+++ quicksort.py
@@ -1,10 +1,13 @@
 #Quicksort Implementaions with Python
 
+import random
+
 def quicksort(array):
     if len(array) <= 1:
         return array
     else:
-        pivot = array[0]
+        #This line always chooses a randow index of the array/sub-array as the pivot to improve the runtime
+        pivot = array[random.randint(0, len(array) -1)]
         less = [i for i in array[1:] if i <= pivot]
         greater = [i for i in array[1:] if i > pivot]
 
