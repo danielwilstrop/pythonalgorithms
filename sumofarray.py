c461f6eca9f3ceb43acd591585e6f16cdebd79b3
Daniel Wilstrop <daniel.wilstrop@outlook.com>
2021-08-04 21:24:04 +0100

    legnth of list added to sumofarray

--- sumofarray.py
+++ sumofarray.py
@@ -7,5 +7,14 @@ def sum(arr):
         return arr[0] + sum(arr[1:])
 
 
+# Recursive funtion to count length of a list, a copy of the built in Len() function
+def len_of_list(arr):
+    if arr == []:
+        return 0
+    else:
+        return 1 + len_of_list(arr[1:])
+
+
 #Tests
 print(sum([1,2,3,4,5]))  #Prints 15
+print(len_of_list([1,2,3,4,5]))  #Prints 15
