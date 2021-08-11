47ec9fe4cab7c581fcbc74efb62371adf2c72617
Daniel Wilstrop <daniel.wilstrop@outlook.com>
2021-08-11 20:52:36 +0100

    quicksort changes

--- quicksort.py
+++ quicksort.py
@@ -1,12 +1,30 @@
 #Quicksort Implementaions with Python
 import random
 
-def quicksort(array):
+# with random index
+def quicksort(arr): 
+    if (len(arr) <= 1):
+        return arr
+    else:
+        grt_arr = []
+        less_arr = []
+        rand_indx = random.randint(0,len(arr)-1)    
+        pivot = arr[rand_indx] # picking up a random index
+        for ele in (arr[0:rand_indx]+arr[rand_indx+1:]):
+            if (ele <= pivot):
+                less_arr.append(ele)
+            elif (ele > pivot):
+                grt_arr.append(ele)
+
+    return quicksort(less_arr)+[pivot]+quicksort(grt_arr)
+
+
+#Without random pivot
+def quicksort1(array):
     if len(array) <= 1:
         return array
     else:
-        random_index = random.randint(0, len(array) -1)
-        pivot = array[random_index]
+        pivot = array[0]
         less = [i for i in array[1:] if i <= pivot]
         greater = [i for i in array[1:] if i > pivot]
 
