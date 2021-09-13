d172a484189772d1afaff7a0ebeec237769b9f75
Daniel Wilstrop <daniel.wilstrop@outlook.com>
2021-09-13 19:46:40 +0100

    Basic times table printer added

--- times_tables.py
+++ times_tables.py
@@ -1,10 +1,5 @@
-from turtle import *
-import turtle
-
 size = 20
 for row in range(1, size + 1):
     for column in range(1, size + 1):
         print(row * column, end = ' ')
     print()
-
-# turtle.mainloop()
